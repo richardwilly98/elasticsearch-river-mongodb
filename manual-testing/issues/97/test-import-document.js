4bb8cfa4eab1fcb9a06238a94582b546d6cf4945
Richard Louapre <richard.louapre@gmail.com>
2013-11-03 20:01:43 -0500

    Fix typo
    - Add js script to find and switch to master

--- manual-testing/issues/97/test-import-document.js
+++ manual-testing/issues/97/test-import-document.js
@@ -1,3 +1,9 @@
+var status = rs.status()
+for (i=0; i < status.members.length; i++) {
+  if (status.members[i].state == 1) {
+    db = connect(status.members[i].name + "/local")
+  }
+}
 use mydb97
 var student = {"name": "joe", "scores": [54]}
 db.mycollec97.save(student)
