09ecc7d46782d23827acb58ea8e767408f8bb21e
Richard Louapre <richard.louapre@gmail.com>
2013-11-12 17:53:06 -0500

    Add script to find master node in MongoDB

--- manual-testing/issues/26/test-issue-26.js
+++ manual-testing/issues/26/test-issue-26.js
@@ -1,3 +1,10 @@
+var status = rs.status()
+ for (i=0; i < status.members.length; i++) {
+   if (status.members[i].state == 1) {
+     db = connect(status.members[i].name + "/local")
+   }
+ }
+
 use mydb
 var o =
 {
