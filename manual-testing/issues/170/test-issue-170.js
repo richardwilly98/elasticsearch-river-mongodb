77a75e7f102509f2511cc37054a9cf5e794105e1
Richard Louapre <richard.louapre@gmail.com>
2013-12-02 05:50:37 -0500

    New test for issue #170
    - Show how to use bulk concurrent request parameter

---
+++ manual-testing/issues/170/test-issue-170.js
@@ -0,0 +1,19 @@
+var status = rs.status()
+ for (i=0; i < status.members.length; i++) {
+   if (status.members[i].state == 1) {
+     db = connect(status.members[i].name + "/local")
+   }
+ }
+
+var max = 5000000;
+use mydb170
+for (var i=0; i < max; i++) {
+  var o = {
+    "firstName": "John",
+    "lastName": "Doe",
+    "employeeId": i,
+    "title": "Developer"
+  }
+  db.mycollection170.save(o);
+  print("save object " + o["_id"]);
+}
