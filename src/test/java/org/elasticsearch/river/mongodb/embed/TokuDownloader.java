69d68911b9c21bc3a15cc6e4fc0acfdd85aa96ad
Kevin D. Keck <kevin@connectifier.com>
2014-05-02 14:58:21 -0700

    Modify copied Downloader source to define TokuDownloader

--- src/test/java/org/elasticsearch/river/mongodb/embed/TokuDownloader.java
+++ src/test/java/org/elasticsearch/river/mongodb/embed/TokuDownloader.java
@@ -1,24 +1,7 @@
 /**
- * Copyright (C) 2011
- *   Michael Mosmann <michael@mosmann.de>
- *   Martin Jöhren <m.joehren@googlemail.com>
- *
- * with contributions from
- *  konstantin-ba@github,Archimedes Trajano (trajano@github)
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Derived from de.flapdoodle.embed.process.store.Downloader
  */
-package de.flapdoodle.embed.process.store;
+package org.elasticsearch.river.mongodb.embed;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
@@ -26,52 +9,103 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.HttpCookie;
+import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLConnection;
+import java.util.List;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.testng.util.Strings;
+
+import com.google.common.base.Joiner;
 
 import de.flapdoodle.embed.process.config.store.IDownloadConfig;
 import de.flapdoodle.embed.process.config.store.ITimeoutConfig;
 import de.flapdoodle.embed.process.distribution.Distribution;
 import de.flapdoodle.embed.process.io.file.Files;
 import de.flapdoodle.embed.process.io.progress.IProgressListener;
+import de.flapdoodle.embed.process.store.ArtifactStore;
+import de.flapdoodle.embed.process.store.IDownloader;
 
 /**
  * Class for downloading runtime
  */
-public class Downloader {
+public class TokuDownloader implements IDownloader {
 
   static final int DEFAULT_CONTENT_LENGTH = 20 * 1024 * 1024;
   static final int BUFFER_LENGTH = 1024 * 8 * 8;
   static final int READ_COUNT_MULTIPLIER = 100;
+  private static Logger logger = Logger.getLogger(ArtifactStore.class.getName());
+  private static final Pattern REFRESH_HEADER_PATTERN = Pattern.compile("^\\d+; url=(.*)");
 
-  private Downloader() {
+  public TokuDownloader() {
 
   }
 
-  public static String getDownloadUrl(IDownloadConfig runtime, Distribution distribution) {
+  public String getDownloadUrl(IDownloadConfig runtime, Distribution distribution) {
     return runtime.getDownloadPath().getPath(distribution) + runtime.getPackageResolver().getPath(distribution);
   }
 
-  public static File download(IDownloadConfig runtime, Distribution distribution) throws IOException {
+  public File download(IDownloadConfig runtime, Distribution distribution) throws IOException {
 
     String progressLabel = "Download " + distribution;
     IProgressListener progress = runtime.getProgressListener();
     progress.start(progressLabel);
 
+    // First get PHPSESSID cookie from download URL
+    ITimeoutConfig timeoutConfig = runtime.getTimeoutConfig();
+
+    URL url = new URL(getDownloadUrl(runtime, distribution));
+    logger.fine("Distro URL: " + url);
+    HttpURLConnection openConnection = (HttpURLConnection) url.openConnection();
+    openConnection.setRequestProperty("User-Agent", runtime.getUserAgent());
+    openConnection.setConnectTimeout(timeoutConfig.getConnectionTimeout());
+    openConnection.setReadTimeout(runtime.getTimeoutConfig().getReadTimeout());
+    openConnection.setInstanceFollowRedirects(false);
+
+    openConnection.getContent(); // we only care about the cookies, not the redirect page contents
+    List<HttpCookie> cookies = HttpCookie.parse(openConnection.getHeaderField("Set-Cookie"));
+    logger.fine("Cookies fetched from distro URL (before redirects): " + cookies);
+    while (("" + openConnection.getResponseCode()).startsWith("30")) {
+      url = new URL(url, openConnection.getHeaderField("Location"));
+      logger.finest("following redirect to: " + url);
+      openConnection = (HttpURLConnection) url.openConnection();
+      openConnection.setRequestProperty("User-Agent", runtime.getUserAgent());
+      openConnection.setConnectTimeout(timeoutConfig.getConnectionTimeout());
+      openConnection.setReadTimeout(runtime.getTimeoutConfig().getReadTimeout());
+      openConnection.setInstanceFollowRedirects(false);
+      openConnection.getContent();
+    }
+    logger.finest("Headers returned from distro URL (after following redirects): " + openConnection.getHeaderFields());
+    String refresh = openConnection.getHeaderField("Refresh");
+    openConnection.getInputStream().close();
+
+    // Then fetch actual file, using the received cookies
     File ret = Files.createTempFile(runtime.getFileNaming()
         .nameFor(runtime.getDownloadPrefix(), "." + runtime.getPackageResolver().getArchiveType(distribution)));
+    logger.fine("Saving distro to " + ret.getAbsolutePath());
     if (ret.canWrite()) {
 
       BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(ret));
 
-      URL url = new URL(getDownloadUrl(runtime, distribution));
-      URLConnection openConnection = url.openConnection();
+      URL url2 = new URL("http://www.tokutek.com/download.php?df=1");
+      if (!Strings.isNullOrEmpty(refresh)) {
+        Matcher m = REFRESH_HEADER_PATTERN.matcher(refresh);
+        if (m.matches()) {
+          url2 = new URL(url, m.group(1));
+        }
+      }
+      logger.fine("Ultimate download URL: " + url2);
+      openConnection = (HttpURLConnection) url2.openConnection();
       openConnection.setRequestProperty("User-Agent",runtime.getUserAgent());
       
-      ITimeoutConfig timeoutConfig = runtime.getTimeoutConfig();
-      
       openConnection.setConnectTimeout(timeoutConfig.getConnectionTimeout());
-      openConnection.setReadTimeout(runtime.getTimeoutConfig().getReadTimeout());
+      openConnection.setReadTimeout(timeoutConfig.getReadTimeout());
+      if (!cookies.isEmpty()) {
+        openConnection.setRequestProperty("Cookie", Joiner.on("; ").join(cookies));
+      }
 
       InputStream downloadStream = openConnection.getInputStream();
 
