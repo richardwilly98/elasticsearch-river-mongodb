caf7331339563a054d168cc2355953faec66f7ab
Ben McCann <ben@connectifier.com>
2014-11-07 18:25:45 -0800

    Add a TODO to include fix we've sent to the embedded mongo project

--- src/test/java/de/flapdoodle/embed/process/extract/FilesToExtract2.java
+++ src/test/java/de/flapdoodle/embed/process/extract/FilesToExtract2.java
@@ -1,7 +1,3 @@
-/**
- * Put all extracted files in a new unique temp dir, to ensure there are no collisions between extractions.
- * The original FilesToExtract tries to write libraries to fixed paths.
- */
 package de.flapdoodle.embed.process.extract;
 
 import java.io.File;
@@ -11,6 +7,15 @@ import de.flapdoodle.embed.process.config.store.FileSet;
 import de.flapdoodle.embed.process.io.directories.IDirectory;
 import de.flapdoodle.embed.process.io.file.Files;
 
+/**
+ * Put all extracted files in a new unique temp dir, to ensure there are no collisions between extractions.
+ * The original FilesToExtract tries to write libraries to fixed paths.
+ * 
+ * TODO: See if we can delete this class after our fix for CachingArtifactStore is committed and we
+ * upgrade to a version of the library containing the fix
+ * https://github.com/flapdoodle-oss/de.flapdoodle.embed.mongo/pull/104
+ * "mvn test" fails without this class at the moment
+ */
 public class FilesToExtract2 extends FilesToExtract {
 
     public FilesToExtract2(IDirectory dirFactory, ITempNaming executableNaming, FileSet fileSet) throws IOException {
